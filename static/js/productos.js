872e09b5ad33f42c5e9f97df948eb4eae7faca1e
JimenaAburtoR <yojana.aburto@vallegrande.edu.pe>
2025-11-18 18:48:58 +0000

    fix:temporary change to the button effect when clicking on products.js

--- static/js/productos.js
+++ static/js/productos.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log("Productos cargado");
 
   const botones = document.querySelectorAll('.product-card button');
+
   botones.forEach(btn => {
     btn.addEventListener('click', () => {
+
+      // ✨ Cambio leve: efecto rápido al presionar el botón
+      btn.classList.add('btn-click');
+      setTimeout(() => btn.classList.remove('btn-click'), 200);
+
       btn.closest('.product-card').classList.add('fade-in');
       alert("Producto agregado al carrito 🛒");
     });
